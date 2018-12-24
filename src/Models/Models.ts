caf9355b3441d8c45202b02dc658e52e4c808f85
Nitin patil <nitinp@bitsdev.in>
2018-12-24 12:03:45 +0530

    new

---
+++ src/Models/Models.ts
@@ -0,0 +1,21 @@
+export class Quotes {
+
+  shareCount: number;
+  likeCount: number;
+  viewCount: number;
+  topics: Array<any>[];
+  auther: Auther
+  quote: string;
+  id:string;
+}
+
+export class Auther {
+
+  died: string;
+  born: string;
+  profession: string;
+  intro: string;
+  name: string;
+  id: string;
+}
+
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import { HttpErrorResponse } from '@angular/common/http';
+import {Auther,Quotes} from './../Models/Models'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +9,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'docp';
+  quotes:Array<Quotes>[];
+  constructor (private http: HttpClient) { }
+  ngOnInit () {
+    this.http.get('./assets/quotes.json').subscribe(
+      data => {
+        this.quotes = data as Array<Quotes>[];	 // FILL THE ARRAY WITH DATA.
+        console.log(this.quotes);
+       
+        console.log(this.quotes[1]);
+      },
+      (err: HttpErrorResponse) => {
+        console.log (err.message);
+      }
+    );
+  }
 }
